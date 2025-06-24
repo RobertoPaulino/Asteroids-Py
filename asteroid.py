51348ca115dde52c55392fbbe39aecfa0fd4aaeb
Robert <rpaulinodev@gmail.com>
2025-06-24 16:45:40 -0400

    adds ability to destroy asteroids

--- asteroid.py
+++ asteroid.py
@@ -1,6 +1,8 @@
+import random
 import pygame
 from circleshape import CircleShape
-
+from constants import ASTEROID_MIN_RADIUS
+from encodings import rot_13
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
@@ -11,3 +13,21 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position += (self.velocity * dt)
+
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+
+        angle = random.uniform(20,50)
+
+        rot_1 = self.velocity.rotate(angle)
+        rot_2 = self.velocity.rotate(-angle)
+
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+
+        asteroid = Asteroid(self.position.x, self.position.y, new_radius)
+        asteroid.velocity = rot_1 * 1.2
+
+        asteroid = Asteroid(self.position.x, self.position.y, new_radius)
+        asteroid.velocity = rot_2 * 1.2
